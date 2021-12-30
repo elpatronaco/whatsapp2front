140fca44623c81d580ecdb034bdf1c462cb64b6d
Pau Colomé Diaz <pau.colome@neoris.com>
2021-12-30 10:57:12 +0100

    Added "trademark"

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,48 +1,66 @@
-import {Component, OnDestroy} from "@angular/core";
-import {NgForm} from "@angular/forms";
-import {Subscription} from "rxjs";
-import {IUserAuthenticate} from "../models/Dto/User/IUserAuthenticate";
-import {LoginRequest} from "../store/global/global.actions";
-import {Store} from "@ngrx/store";
-
+import { Component, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { IUserAuthenticate } from '../models/Dto/User/IUserAuthenticate';
+import { LoginRequest } from '../store/global/global.actions';
+import { Store } from '@ngrx/store';
 
 @Component({
-  selector: "app-landing",
-  styleUrls: ["./landing.component.sass"],
+  selector: 'app-landing',
+  styleUrls: ['./landing.component.sass'],
   template: `
     <div class="landing__wrapper">
       <header class="landing__header">
-        <img class="icon" src="assets/icon.svg" alt="whatsapp 2 logo">
+        <img class="icon" src="assets/icon.svg" alt="whatsapp 2 logo" />
         <p>WHATSAPP 2 WEB</p>
       </header>
       <section class="landing__window">
         <h3>Inicia sesión</h3>
         <form class="login-form" #f="ngForm" (ngSubmit)="onSubmit(f)">
-          <div class="form-group"><label for="phoneInput">Teléfono</label><input id="phoneInput" name="phone"
-                                                                                 type="tel"
-                                                                                 required
-                                                                                 tabindex="1"
-                                                                                 [(ngModel)]="loginForm.phone">
+          <div class="form-group">
+            <label for="phoneInput">Teléfono</label
+            ><input
+              id="phoneInput"
+              name="phone"
+              type="tel"
+              required
+              tabindex="1"
+              [(ngModel)]="loginForm.phone"
+            />
           </div>
-          <div class="form-group"><label for="passwordInput">Contraseña</label><input id="passwordInput"
-                                                                                      name="password"
-                                                                                      type="password" required
-                                                                                      minlength="8"
-                                                                                      tabindex="2"
-                                                                                      [(ngModel)]="loginForm.password">
+          <div class="form-group">
+            <label for="passwordInput">Contraseña</label
+            ><input
+              id="passwordInput"
+              name="password"
+              type="password"
+              required
+              minlength="8"
+              tabindex="2"
+              [(ngModel)]="loginForm.password"
+            />
           </div>
-          <button type="submit" [disabled]="f.invalid" tabindex="3">Enviar</button>
+          <button type="submit" [disabled]="f.invalid" tabindex="3">
+            Enviar
+          </button>
         </form>
       </section>
+      <footer class="landing__footer">
+        <div>
+          Made with <span>&hearts;</span> by
+          <a href="https://github.com/elpatronaco" target="_blank"
+            >Pau Colomé</a
+          >
+        </div>
+      </footer>
     </div>
-  `
+  `,
 })
 export class LandingComponent implements OnDestroy {
-  loginForm: IUserAuthenticate = {phone: '', password: ''};
+  loginForm: IUserAuthenticate = { phone: '', password: '' };
   loginSubscription: Subscription | undefined;
 
-  constructor(private store: Store) {
-  }
+  constructor(private store: Store) {}
 
   ngOnDestroy(): void {
     this.loginSubscription?.unsubscribe();
